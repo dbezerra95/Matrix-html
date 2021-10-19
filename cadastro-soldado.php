8786e93ea89d8a78bc0112d5da3b1ca0436f3f16
DanielBCastro <dbezerra106@gmail.com>
2021-10-19 01:01:13 -0300

    Revive2

--- cadastro-soldado.php
+++ cadastro-soldado.php
@@ -36,7 +36,7 @@
                 <a class="navbar-brand">Matrix Menu</a>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#principal">Home</a>
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#quemsomos">Quem Somos</a>
